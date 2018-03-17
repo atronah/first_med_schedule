8ad662e070937a301da07388bd2cc96c226c8ea3
atronah <atronah.ds@gmail.com>
2018-03-17 16:03:36 +0300

    ft(schedule): adds script to build fms.ics file
    Usage: python3 schedule.py group_code

---
+++ schedule.py
@@ -0,0 +1,58 @@
+import sys
+import requests
+import logging
+from datetime import datetime, timedelta
+
+from icalendar import Calendar, Timezone, TimezoneStandard, Event
+from lxml import html
+
+schedule_url = f'http://www.1spbgmu.ru/ruz/?timetable&group={sys.argv[1]}'
+
+html_source = requests.get(schedule_url).text
+
+schedule = html.fromstring(html_source)
+
+cal = Calendar()
+
+# timezone info
+tz = Timezone()
+tz.add('TZID', '(UTC+03:00) Moscow, St. Petersburg, Volgograd')
+tz_st = TimezoneStandard()
+tz_st.add('DTSTART', datetime(1601, 1, 1))
+tz_st.add('TZOFFSETFROM', timedelta(hours=4))
+tz_st.add('TZOFFSETTO', timedelta(hours=3))
+tz.add_component(tz_st)
+cal.add_component(tz)
+
+for day_info in schedule.xpath("//div[contains(@class, 'list')]"):
+    day_of_week_node = day_info.xpath("*[contains(@class, 'dayofweek')]")
+    try:
+        event_date = datetime.strptime(day_of_week_node[0].text.split(',')[1].strip()
+                                       , '%d.%m.%Y').date()
+    except (IndexError, ValueError):
+        logging.warning(f'Incorrect day of week info: {day_of_week_node}')
+        continue
+
+    for event_info in day_info.xpath("*[contains(@class, 'timetable_sheet')"
+                                     " and contains(@class, 'visible')]"):
+        event = Event()
+        event.add('DTSTAMP', datetime.now())
+
+        time_interval = event_info.xpath("span[contains(@class, 'time_para')]")[0].text
+        event_start, event_end = map(lambda s: datetime.strptime(s.strip(), '%H:%M').time(),
+                                     time_interval.split('–'))
+        event.add('DTSTART', datetime.combine(event_date, event_start))
+        event.add('DTEND', datetime.combine(event_date, event_end))
+        event.add('LOCATION', event_info.xpath("span[contains(@class, 'auditorium')]")[0].text)
+        event.add('SUMMARY', event_info.xpath("span[contains(@class, 'discipline')]")[0].text)
+
+        kind = event_info.xpath("span[contains(@class, 'kindOfWork')]")[0].text
+        lecturer = event_info.xpath("span[contains(@class, 'lecturer')]")[0].text
+        group_info = event_info.xpath("span[contains(@class, 'group')]")
+        group = group_info[0].text if group_info else ''
+        event.add('DESCRIPTION',
+                  '\n'.join([kind, lecturer, group]))
+        cal.add_component(event)
+
+with open('fms.ics', 'w+b') as ics:
+    ics.write(cal.to_ical())
